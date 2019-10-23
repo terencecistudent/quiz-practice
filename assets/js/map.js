d354a437a75efe39be7af083a68474059be2fc71
terencecistudent <terencelogue100@hotmail.co.uk>
2019-10-23 17:13:30 +0000

    map styles added and functions added in map js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,9 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 3,
         center: {
-            lat: 52.355518,
-            lng: -1.174320
+            lat: 53.794973,
+            lng: -4.688159
         }
     });
 
@@ -23,7 +23,7 @@ function initMap() {
         {lat: 53.430868, lng: -2.960830},
         {lat: 53.483196, lng: -2.200395},
         {lat: 53.463097, lng: -2.291329},
-        {lat: 54.975279, lng: 1.622890},
+        {lat: 54.975673, lng: -1.621667},
         {lat: 52.622166, lng: 1.309175},
         {lat: 53.370393, lng: -1.470901},
         {lat: 50.905887, lng: -1.390973},
